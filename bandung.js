94bb822eba55414fd9ab0fc341f9bfc6e42b1d38
aesel30 <145676659+aesel30@users.noreply.github.com>
2024-07-22 23:42:31 +0700

    Update bandung.js

--- bandung.js
+++ bandung.js
@@ -1,44 +1,39 @@
 // ==UserScript==
 // @name         Bandung 
-// @namespace    https://viayoo.com/
+// @namespace    https://aesel.github.io
 // @version      0.1
 // @description  try to take over the world!
-// @author       Faizin
+// @author       Aesel
 // @run-at       document-start
 // @match        https://j6.prometric-jp.com/Reserve/SelectPlace*
 // @grant        none
 // ==/UserScript==
 
-window.addEventListener("DOMContentLoaded",(c=>{"use strict";var i=!1;['JavaScript:onClick=click_next(\\"IOJ04\\",\\"09:15\\",\\"1\\")','JavaScript:onClick=click_next(\\"IOJ04\\",\\"10:00\\",\\"1\\")','JavaScript:onClick=click_next(\\"IOJ04\\",\\"10:45\\",\\"1\\")','JavaScript:onClick=click_next(\\"IOJ04\\",\\"11:30\\",\\"1\\")','JavaScript:onClick=click_next(\\"IOJ04\\",\\"12:15\\",\\"1\\")','JavaScript:onClick=click_next(\\"IOJ04\\",\\"13:00\\",\\"1\\")','JavaScript:onClick=click_next(\\"IOJ04\\",\\"09:15\\",\\"2\\")','JavaScript:onClick=click_next(\\"IOJ04\\",\\"10:00\\",\\"2\\")','JavaScript:onClick=click_next(\\"IOJ04\\",\\"10:45\\",\\"2\\")','JavaScript:onClick=click_next(\\"IOJ04\\",\\"11:30\\",\\"2\\")','JavaScript:onClick=click_next(\\"IOJ04\\",\\"12:15\\",\\"2\\")','JavaScript:onClick=click_next(\\"IOJ04\\",\\"13:00\\",\\"2\\")'].forEach((function(c){if(!i){var t=document.querySelectorAll('a[href="'+c+'"]');t.length>0&&(t[0].click(),i=!0)}}))}))();
+window.addEventListener("DOMContentLoaded", () => {
+    "use strict";
+    const links = [
+        'JavaScript:onClick=click_next("IOJ04","09:15","1")',
+        'JavaScript:onClick=click_next("IOJ04","10:00","1")',
+        'JavaScript:onClick=click_next("IOJ04","10:45","1")',
+        'JavaScript:onClick=click_next("IOJ04","11:30","1")',
+        'JavaScript:onClick=click_next("IOJ04","12:15","1")',
+        'JavaScript:onClick=click_next("IOJ04","13:00","1")',
+        'JavaScript:onClick=click_next("IOJ04","09:15","2")',
+        'JavaScript:onClick=click_next("IOJ04","10:00","2")',
+        'JavaScript:onClick=click_next("IOJ04","10:45","2")',
+        'JavaScript:onClick=click_next("IOJ04","11:30","2")',
+        'JavaScript:onClick=click_next("IOJ04","12:15","2")',
+        'JavaScript:onClick=click_next("IOJ04","13:00","2")'
+    ];
 
-[   'JavaScript:onClick=click_next(\\"IOJ04\\",\\"09:15\\",\\"1\\")',
-    'JavaScript:onClick=click_next(\\"IOJ04\\",\\"10:00\\",\\"1\\")',
-    'JavaScript:onClick=click_next(\\"IOJ04\\",\\"10:45\\",\\"1\\")',
-    'JavaScript:onClick=click_next(\\"IOJ04\\",\\"11:30\\",\\"1\\")',
-    'JavaScript:onClick=click_next(\\"IOJ04\\",\\"12:15\\",\\"1\\")',
-    'JavaScript:onClick=click_next(\\"IOJ04\\",\\"13:00\\",\\"1\\")',
-    'JavaScript:onClick=click_next(\\"IOJ04\\",\\"09:15\\",\\"2\\")',
-    'JavaScript:onClick=click_next(\\"IOJ04\\",\\"10:00\\",\\"2\\")',
-    'JavaScript:onClick=click_next(\\"IOJ04\\",\\"10:45\\",\\"2\\")',
-    'JavaScript:onClick=click_next(\\"IOJ04\\",\\"11:30\\",\\"2\\")',
-    'JavaScript:onClick=click_next(\\"IOJ04\\",\\"12:15\\",\\"2\\")',
-    'JavaScript:onClick=click_next(\\"IOJ04\\",\\"13:00\\",\\"2\\")']
-
-[
-    {
-      "links": [
-        "JavaScript:onClick=click_next(\"IOJ04\",\"09:15\",\"1\")",
-        "JavaScript:onClick=click_next(\"IOJ04\",\"10:00\",\"1\")",
-        "JavaScript:onClick=click_next(\"IOJ04\",\"10:45\",\"1\")",
-        "JavaScript:onClick=click_next(\"IOJ04\",\"11:30\",\"1\")",
-        "JavaScript:onClick=click_next(\"IOJ04\",\"12:15\",\"1\")",
-        "JavaScript:onClick=click_next(\"IOJ04\",\"13:00\",\"1\")",
-        "JavaScript:onClick=click_next(\"IOJ04\",\"09:15\",\"2\")",
-        "JavaScript:onClick=click_next(\"IOJ04\",\"10:00\",\"2\")",
-        "JavaScript:onClick=click_next(\"IOJ04\",\"10:45\",\"2\")",
-        "JavaScript:onClick=click_next(\"IOJ04\",\"11:30\",\"2\")",
-        "JavaScript:onClick=click_next(\"IOJ04\",\"12:15\",\"2\")",
-        "JavaScript:onClick=click_next(\"IOJ04\",\"13:00\",\"2\")"
-    
-    }
-  ]
+    let clicked = false;
+    links.forEach(link => {
+        if (!clicked) {
+            const elements = document.querySelectorAll(`a[href="${link}"]`);
+            if (elements.length > 0) {
+                elements[0].click();
+                clicked = true;
+            }
+        }
+    });
+});
