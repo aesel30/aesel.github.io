18395757f9cf7aefbc2c421ec7cb55374bd800b6
aesel30 <145676659+aesel30@users.noreply.github.com>
2024-07-27 05:43:16 +0700

    Update pm.js

--- pm.js
+++ pm.js
@@ -11,26 +11,26 @@
 
 window.onload = function() {
     "use strict";
-    
+
     // ID dan Password
-    var user = "JP";
-    var sandi = "";
-    
+    const user = "";
+    const sandi = "";
+
     // Tanggal lahir
-    var Tahun = "";
-    var Bulan = "";
-    var Tanggal = "";
-    
+    const Tahun = "";
+    const Bulan = "";
+    const Tanggal = "";
+
     // Jenis kelamin (isi cowo atau cewe)
-    var Gender = "cowo";
-    
+    const Gender = "cewe";
+
     // Login page
     if (document.location.href.indexOf("https://j6.prometric-jp.com/Reserve/Login") > -1) {
         document.querySelector('input[name="id_number"]').value = user;
         document.querySelector('input[name="password"]').value = sandi;
         document.querySelector('input[name="captchaCode"]').style.textTransform = "uppercase";
     }
-    
+
     // Private page
     else if (document.location.href.indexOf("https://j6.prometric-jp.com/Reserve/PrivateChk") > -1) {
         document.querySelector('input[type="button"][name="Continue"]').click();
@@ -45,25 +45,25 @@ window.onload = function() {
     else if (document.location.href.indexOf("https://j6.prometric-jp.com/Reserve/Attention") > -1) {
         document.querySelector('input[onclick="agree(\'ExamSelect\')"]').click();
     }
-    
+
     // ExamSelect page
     else if (document.location.href.indexOf("https://j6.prometric-jp.com/Reserve/ExamSelect") > -1) {
         document.querySelector('option[value="T20-J11J"]').selected = true;
         document.querySelector('input[name="test"]').click();
     }
-    
+
     // Status page
     else if (document.location.href.indexOf("https://j6.prometric-jp.com/Reserve/Status") > -1) {
         document.querySelectorAll('select[name="selBYear"] option[value="'+Tahun+'"]').forEach(e => e.selected = true);
         document.querySelectorAll('select[name="selBMonth"] option[value="'+Bulan+'"]').forEach(e => e.selected = true);
         document.querySelectorAll('select[name="selBDay"] option[value="'+Tanggal+'"]').forEach(e => e.selected = true);
-        
+
         if (Gender === "cewe") {
             document.querySelector('input[name="rdoGender"][value="1"]').checked = true;
         } else if (Gender === "cowo") {
             document.querySelector('input[name="rdoGender"][value="2"]').checked = true;
         }
-        
+
         document.querySelector('input[name="rdoNation"]').checked = true;
         document.querySelector('option[value="Indonesia"]').selected = true;
         document.querySelector('option[value="No occupation"]').selected = true;
@@ -80,14 +80,13 @@ window.onload = function() {
 
 window.addEventListener("DOMContentLoaded", (e) => {
     "use strict";
-    
+
     // Auto search (tambah "," untuk memisahkan tgl contoh 12,19,20)
-    var Tahun1 = "2024";
-    var Bulan1 = "08";
-    var Tanggal1 = [1, 2, 3, 4, 5];
+    const Tahun1 = "2024";
+    const Bulan1 = "07";
+    const Tanggal1 = [31];
     // No HP untuk pembayaran
-    var noHP = "081899998888";
-    
+    const noHP = "08985152024";
     // Discount page
     if (document.location.href.indexOf("https://j6.prometric-jp.com/Reserve/Discount") > -1) {
         document.querySelector('input[value="zotapay"]').checked = true;
@@ -99,7 +98,7 @@ window.addEventListener("DOMContentLoaded", (e) => {
         document.querySelector('input[name="tel_1"]').value = noHP;
         document.querySelector('input[name="next"]').click();
     }
-    
+
     // SelectPlace page
     else if (document.location.href.indexOf("https://j6.prometric-jp.com/Reserve/SelectPlace") > -1) {
         const e = Tanggal1;
@@ -111,7 +110,7 @@ window.addEventListener("DOMContentLoaded", (e) => {
             document.querySelector('input[name="exam_day_d"]').value = e;
         });
     }
-    
+
     // SelectPlace result page
     else if (document.location.href.indexOf("https://j6.prometric-jp.com/Reserve/SelectPlace#result") > -1) {
         const e = Tanggal1;
@@ -126,5 +125,5 @@ window.addEventListener("DOMContentLoaded", (e) => {
 
     setTimeout(() => {
             document.querySelector('input[name="search"]').click();
-        }, 800);
+        }, 100);
 });
